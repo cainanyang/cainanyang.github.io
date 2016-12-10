d5080c2b291e5155658cbc11104e5625476375f3
蔡南洋 <cainanyang@cainanyang-p.lan>
2016-12-11 06:30:15 +0800

    pelican init

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'cainanyang'
+SITENAME = u'南洋叔叔的小屋'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('蒙氏街', 'http://www.mengshij.com/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('github', 'http://github.com/cainanyang'),
+)
+
+DEFAULT_PAGINATION = 100
+
+GOOGLE_ANALYTICS = 'UA-87012222-1'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
